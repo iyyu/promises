d546b760220dfdcb6a7b373b92b774b9340f67db
Iris Yu <iyucomms@gmail.com>
2017-12-25 16:44:00 -0800

    Implement combineFirstLineOfManyFiles using Promise.all

--- exercises/advanced/collections.js
+++ exercises/advanced/collections.js
@@ -8,9 +8,22 @@
  *    4. Writes the new file to the file located at `writePath`
  */
 
+var Promise = require('bluebird');
+var pluckFirstLineFromFileAsync = require('../bare_minimum/promiseConstructor').pluckFirstLineFromFileAsync;
+var fs = require('fs');
+var writeFileAsync = Promise.promisify(fs.writeFile);
 
 var combineFirstLineOfManyFiles = function(filePaths, writePath) {
- // TODO
+  var results = [];
+  for (var i = 0; i < filePaths.length; i++) {
+    results.push(pluckFirstLineFromFileAsync(filePaths[i]));
+  }
+  return Promise.all(results).then(function(lines) {
+    writeFileAsync(writePath, lines.join('\n'));
+  })
+  // create a results array
+  // push promises of each file at the path
+ // return Promise.all(results)
 };
 
 // Export these functions so we can unit test them
